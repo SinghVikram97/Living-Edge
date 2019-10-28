859f1553f14d8b9e29cf46ff1eeb5324eed38d75
Vikram Singh Bedi <vikram.bedi97@gmail.com>
2019-10-28 23:57:18 +0530

    Updates

---
+++ precache-manifest.0394761592e96ac42226fab9f6929088.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77217555ddf9143acf22a4e99a034aa1",
+    "url": "/Living-Edge/index.html"
+  },
+  {
+    "revision": "11613e9675ff6f8d2f30",
+    "url": "/Living-Edge/static/css/main.747d594f.chunk.css"
+  },
+  {
+    "revision": "3aa818bb941c71e03f6c",
+    "url": "/Living-Edge/static/js/2.fc4c70c0.chunk.js"
+  },
+  {
+    "revision": "11613e9675ff6f8d2f30",
+    "url": "/Living-Edge/static/js/main.8a8fc976.chunk.js"
+  },
+  {
+    "revision": "c4e6f9213454f6f5b57a",
+    "url": "/Living-Edge/static/js/runtime-main.503db1dc.js"
+  },
+  {
+    "revision": "cab32bfe58f026237cf0bf267a3d6a31",
+    "url": "/Living-Edge/static/media/c1.cab32bfe.jpg"
+  },
+  {
+    "revision": "d34330d37344141533bfe0c80fa823dd",
+    "url": "/Living-Edge/static/media/c2.d34330d3.jpg"
+  },
+  {
+    "revision": "dd31f6442fe477eccabf662410dcd773",
+    "url": "/Living-Edge/static/media/c3.dd31f644.jpg"
+  },
+  {
+    "revision": "66d578992f15be67f5c712723a78cb44",
+    "url": "/Living-Edge/static/media/c4.66d57899.jpg"
+  },
+  {
+    "revision": "d98488b64ee038c6c3b1ed7856d05328",
+    "url": "/Living-Edge/static/media/p1.d98488b6.jpg"
+  },
+  {
+    "revision": "9062e89a7631482c3219024cb8e6e430",
+    "url": "/Living-Edge/static/media/p6.9062e89a.jpg"
+  }
+]);
